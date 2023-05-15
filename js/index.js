74c90a055fcf48a7a5bd6206fe9acbd17616e4a8
Danny Nhu <132909701+DinhNguyenNhu@users.noreply.github.com>
2023-05-15 11:06:15 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,15 +4,29 @@ let b = 5;
 let c;
 
 // alert( "description of alerted_value: " + alerted_value );
-let output = `
+let print = `
     let a = 3;
     let b = 5;
     let c;
     ----------
     a + b = ${a+b}
     a - b = ${a-b}
+    a * b = ${a*b}
+    a / b = ${a/b}
+    a % b = ${a%b}
+    (a += b): ${a+=b}
+    (a -= b): ${a-=b}
+    (a *= b): ${a*=b}
+    (a /= b): ${a/=b}
+    (a %= b): ${a%=b}
+    (a == b): ${a==b}
+    (a != b): ${a!=b}
+    (a > b): ${a>b}
+    (a < b): ${a<b}
+    (!a && !c): ${!a&&!c}
+    (!a || !c): ${!a||!c}
     `;
-alert(output);
+alert(print);
 /*
 a + b
 a - b
@@ -33,3 +47,12 @@ a < b
 */
 
 //Task 2
+let first_name = "Dinh Nguyen";
+let last_name = "Nhu";
+let email = "nhu00001@algonquinlive.com";
+
+let output;
+
+output = `My name is ${first_name} ${last_name}. You can contact me at ${email}.`;
+alert(output);
+
